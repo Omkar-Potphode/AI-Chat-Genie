05e731c9a957ee861ee923514a908563b34f2cbd
Omkar Potphode <omkar03potphode@gmail.com>
2024-04-23 22:36:42 +0530

    🍱 Added the favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Header from './components/Header'
+
 
 function App() {
 
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden bg-black'>
-        <Header/>
+        header
       </div>
     </>
   )
